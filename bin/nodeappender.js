bd7348cfb074e24a6d69ab23705264ef0c40627e
Paz Weisman <paz.weisman@gmail.com>
2023-01-17 17:53:45 +0200

    more on publishing

---
+++ bin/nodeappender.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+//https://ourcodeworld.com/articles/read/393/how-to-create-a-global-module-for-node-js-properly
+//https://medium.com/jspoint/creating-cli-executable-global-npm-module-5ef734febe32
+import main from '../index.js'
+
+main();
