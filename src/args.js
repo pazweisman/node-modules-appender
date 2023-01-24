8556b954ef748e2348030bab4ac591e8cefc84c3
Paz Weisman <paz.weisman@gmail.com>
2023-01-24 17:07:32 +0200

    more work

--- src/args.js
+++ src/args.js
@@ -36,35 +36,3 @@ const args = yargs(process.argv.slice(2))
 
 const { _, source, target, volume } = args;
 export default {command:_, source, target, volume};
-
-
-// https://markoskon.com/yargs-examples/
-// const usage = yargs(process.argv.slice(2))
-// .scriptName('nodeappend')
-// .usage(`nodeappend -a <action> arguments`)
-// .option("a", { alias: "action", describe: "Action", type: "string", demandOption: true, choices:['append','restore'] })
-// .option("s", { alias: "source", describe: "Source folder", type: "string" })
-// .option("t", { alias: "target", describe: "Target folder", type: "string" })
-// .option("v", { alias: "volume", describe: "Volume size", type: "number", default:5 })
-// .argv;
-
-// usage.source = usage.source ? usage.source.replace(/\\/gi, '/') : null;
-// usage.target = usage.target ? usage.target.replace(/\\/gi, '/') : null;
-
-// if(usage.action === 'append' && !usage.source){
-//     usage.source = nodeModulesPath();
-// }
-
-// if(usage.action === 'append' && !usage.target){
-//     usage.target = path.join(process.cwd(), 'appended');
-// }
-
-// if(usage.action === 'restore' && !usage.source){
-//     usage.target = path.join(process.cwd(), 'appended');
-// }
-
-// if(usage.action === 'restore' && !usage.target){
-//     usage.target = path.join(process.cwd(), 'node_modules');
-// }
-
-// export default usage;
--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@
 import args from './args.js';
 import { append } from './append.js';
 import { restore } from './restore.js';
-import { yellow } from './common.js';
+import { blue } from './common.js';
 
 export function main(){
     
     const logo = `
-                  _                                       _           
-    _ _   ___  __| | ___  __ _  _ __  _ __  ___  _ _   __| | ___  _ _ 
-   | ' \ / _ \/ _' |/ -_)/ _' || '_ \| '_ \/ -_)| ' \ / _' |/ -_)| '_|
-   |_||_|\___/\__,_|\___|\__,_|| .__/| .__/\___||_||_|\__,_|\___||_|  
-                               |_|   |_|                              
-  
-  `;
-    yellow(logo);
-    console.log('args:');
-    console.log(args);
+┌┐┌┌─┐┌┬┐┌─┐┌─┐┌─┐┌─┐┌─┐┌┐┌┌┬┐┌─┐┬─┐
+││││ │ ││├┤ ├─┤├─┘├─┘├┤ │││ ││├┤ ├┬┘
+┘└┘└─┘─┴┘└─┘┴ ┴┴  ┴  └─┘┘└┘─┴┘└─┘┴└─
+    `;
+    blue(logo);
     return;
     try{
         switch(args.command){
